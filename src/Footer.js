d6cf7d6f1dc25f814fb88934fe5a01015c26e689
s4nd0 <piotr28883@gmail.com>
2024-01-18 22:45:44 +0100

    Added icons in the footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,27 @@
 import React from "react";
 
+import facebook from "./images/icons8-facebook.svg";
+import twitter from "./images/icons8-twitter.svg";
+import instagram from "./images/icons8-instagram.svg";
+import github from "./images/icons8-github.svg";
+import tiktok from "./images/icons8-tiktok.svg";
+
 const Footer = () => {
+  const mediaArray = [facebook, instagram, twitter, github, tiktok];
+
   return (
     <footer>
-      &copy; {new Date().getFullYear()} <span>Muscle</span>Mate
+      <p>Visit our social media</p>
+      <div className="footer-media">
+        {mediaArray.map((media) => (
+          <a key={media} href="#">
+            <img src={media} alt={media} />
+          </a>
+        ))}
+      </div>
+      <div>
+        &copy; {new Date().getFullYear()} <span>Muscle</span>Mate
+      </div>
     </footer>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,10 +7,7 @@ const Home = () => {
   return (
     <div className="home">
       <h1>Hello, {user.displayName}!</h1>
-      <p>
-        Your training today will be based on training the upper chest and
-        triceps. Here are your training records from last week
-      </p>
+      <p></p>
     </div>
   );
 };
